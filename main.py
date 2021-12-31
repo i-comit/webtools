79ab82f9cd54e2b2e8f72584a34093f0d1f17f1c
smarttarded <khiemgluong@gmail.com>
2021-12-31 00:02:27 -0800

    removed datadeni script link.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from datadeni import Page1
 from autoping import Page2
 from imgscraper import Page3
 from tablescraper import Page4
@@ -8,7 +7,6 @@ from about import Page5
 class MainView(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
-        p1 = Page1(self)
         p2 = Page2(self)
         p3 = Page3(self)
         p4 = Page4(self)
@@ -19,19 +17,16 @@ class MainView(tk.Frame):
         buttonframe.pack(side="top", fill="x", pady='10', padx='10',expand=False)
         container.pack(side="top", fill="both", expand=True)
 
-        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         p4.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         p5.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
-        b1 = tk.Button(buttonframe, text="DATADENI", command=p1.lift)
         b2 = tk.Button(buttonframe, text="WEBPAGE PINGER", command=p2.lift)
         b3 = tk.Button(buttonframe, text="IMAGE SCRAPER", command=p3.lift)
         b4 = tk.Button(buttonframe, text="TABLE SCRAPER", command=p4.lift)
         b5 = tk.Button(buttonframe, text="ABOUT", command=p5.lift)
 
-        b1.pack(side="left")
         b2.pack(side="left")
         b3.pack(side="left")
         b4.pack(side="left")
